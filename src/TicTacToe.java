bf3439a7e3ffd245bfba77aa36761b19e6b879b1
maxriedel03 <max@m-riedel.de>
2022-02-10 14:49:22 +0100

    Basic Functions

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -24,10 +24,8 @@ public class TicTacToe extends TicTacToeBasis{
         for (int i = 0; i < field.length; i++){
             for (int j = 0; j < field.length; j++){
                 field[i][j] = new TicTacToeButton(100, 100, i*100 + 5, j*100 + 5);
-                //field[i][j].setCurrentPlayer(0);
             }
         }
-
     }
 
     public void updateLabel(){
@@ -36,20 +34,36 @@ public class TicTacToe extends TicTacToeBasis{
 
     public void mouseClicked(MouseEvent e){
         int x, y;
+        boolean won;
         x = e.getX();
         y = e.getY();
         System.out.println("Mouse clicked at: " + x + " " + y);
         mouseClicked(x, y);
-        if(Logic.checkForWin(field)){
+        won = Logic.checkForWin(field);
+        if(won){
             JOptionPane.showMessageDialog(f,
                     "Congrats Player " + playerToMove + " won");
-        }
-        if(playerToMove == 1)
-            playerToMove = PLAYER2;
-        else
-            playerToMove = PLAYER1;
+            restartGame();
+        }else{
+            if(playerToMove == 1)
+                playerToMove = PLAYER2;
+            else
+                playerToMove = PLAYER1;
 
+            updateLabel();
+        }
+    }
+
+    private void restartGame(){
+        field = new TicTacToeButton[3][3];
+        for (int i = 0; i < field.length; i++){
+            for (int j = 0; j < field.length; j++){
+                field[i][j] = new TicTacToeButton(100, 100, i*100 + 5, j*100 + 5);
+            }
+        }
+        playerToMove = 1;
         updateLabel();
+        f.repaint();
     }
 
     private void mouseClicked(int x, int y) {
@@ -71,58 +85,4 @@ public class TicTacToe extends TicTacToeBasis{
             }
         }
     }
-
-
-/*
-    public static void chooseGameStyle(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Play against friend: 1, Play against KI: 2");
-        int playStyle = sc.nextInt();
-        while(playStyle != 1 && playStyle != 2){
-            System.out.println("Keine Zulässige Spielvariante");
-            playStyle = sc.nextInt();
-        }
-        if (playStyle == 1)
-            playGameWithoutKI();
-        else
-            playGameWithKI();
-    }
-
-    public static void playGameWithKI(){
-        boolean gameWon = false;
-        boolean playerWon = false;
-        while(!gameWon){
-            Logic.playerTurn(1, field);
-            Logic.outputField(field);
-            gameWon = Logic.checkForWin(field);
-            if(gameWon){
-                playerWon = true;
-                break;
-            }
-            Logic.kiTurn(field);
-            gameWon = Logic.checkForWin(field);
-        }
-        if(playerWon)
-            System.out.println("Congrats Player 1, you won against the KI!");
-        else
-            System.out.println("You will win next time!");
-    }
-
-
-    public static void playGameWithoutKI(){
-        int playerToTurn = PLAYER1;
-        boolean gameWon = false;
-        while(!gameWon){
-            Logic.playerTurn(playerToTurn, field);
-            Logic.outputField(field);
-            gameWon = Logic.checkForWin(field);
-            if(!gameWon){
-                if(playerToTurn == PLAYER1)
-                    playerToTurn = PLAYER2;
-                else
-                    playerToTurn = PLAYER1;
-            }
-        }
-        System.out.printf("Congrats Player %d you won!", playerToTurn);
-    }*/
 }
